60b667b9f49ce590b420a173979dfb586e0ec9fb
haudd1705 <haudd1705@gmail.com>
2024-02-07 08:45:30 +0700

    pussh

--- src/screens/dashboard/components/UserManage.js
+++ src/screens/dashboard/components/UserManage.js
@@ -99,13 +99,10 @@ const UserManage = () => {
         }
     ]
     const TimeBody = (inputDate) => {
-
         const dateObject = new Date(inputDate);
-
         const day = dateObject.getDate().toString().padStart(2, '0');
         const month = (dateObject.getMonth() + 1).toString().padStart(2, '0');
         const year = dateObject.getFullYear();
-
         const formattedDate = `${day}-${month}-${year}`;
         return (
             <>
